bdc25fcac0968c117902f6695d8f9cd48807b95e
Alexander Plavin <alexander@plav.in>
2022-02-05 21:03:45 +0300

    more options

--- src/VLBIPlots.jl
+++ src/VLBIPlots.jl
@@ -41,53 +41,42 @@ proper_ustrip(::typeof(angle), x) = ustrip(u"°", x)
 proper_label(::typeof(abs)) = "Amplitude (Jy)"
 proper_label(::typeof(angle)) = "Phase (°)"
 
-function radplot(func, mod, c::AbstractVector{<:NamedTuple})
+function radplot(func::Function, mod, c::AbstractVector{<:NamedTuple}; color=:red, kwargs...)
     uvdists = @p c |> map(norm(_.uv))
     vises = @p c |> map(proper_ustrip(func, func(visibility(mod, _.uv))))
     plt.xlim(auto=true)
     plt.ylim(auto=true)
-    plt.scatter(uvdists, vises; s=0.1, c=:red)
+    plt.scatter(uvdists, vises; s=0.1, color, kwargs...)
     plt.xlabel("UV distance (λ)")
     plt.ylabel(proper_label(func))
     plt.xlim(0)
     func == abs && plt.ylim(0)
-    func == angle && ylim_shrink(0±180)
+    func == angle && lim_intersect(y=0 ± 180)
 end
 
-function radplot(func, c::AbstractVector{<:NamedTuple})
+function radplot(func::Function, c::AbstractVector{<:NamedTuple}; color=:k, kwargs...)
     uvdists = @p c |> map(norm(_.uv))
     vises = @p c |> map(proper_ustrip(func, func(_.visibility)))
     plt.xlim(auto=true)
     plt.ylim(auto=true)
-    plt.plot(uvdists, vises, ","; color=:k, zorder=-1)  # zorder=1 is default for scatter, while zorder=2 for plot
+    plt.plot(uvdists, vises, ","; color, zorder=-1, kwargs...)  # zorder=1 is default for scatter, while zorder=2 for plot
     plt.xlabel("UV distance (λ)")
     plt.ylabel(proper_label(func))
     plt.xlim(0)
     func == abs && plt.ylim(0)
-    func == angle && ylim_shrink(0±180)
+    func == angle && lim_intersect(y=0 ± 180)
 end
 
-function radplot(func::typeof(abs), c::ModelComponent, uvrng::AbstractInterval{<:Real}; n=100, alpha=0.2)
+function radplot(func::Function, c::ModelComponent, uvrng::AbstractInterval{<:Real}; n=100, alpha=0.2, color=nothing, kwargs...)
     uvdists = range(extrema(uvrng)...; length=n)
     vises = proper_ustrip.(func, visibility_envelope.(func, c, uvdists))
-    color = plt.gca()._get_lines.prop_cycler.__next__()["color"] |> mpl_color
-    plt.fill_between(uvdists, minimum.(vises), maximum.(vises); ec=color, fc=coloralpha(color, alpha))
+    color = @something(color, plt.gca()._get_lines.prop_cycler.__next__()["color"] |> mpl_color)
+    plt.fill_between(uvdists, minimum.(vises), maximum.(vises); ec=color, fc=coloralpha(color, alpha), kwargs...)
     plt.xlabel("UV distance (λ)")
     plt.ylabel(proper_label(func))
     plt.xlim(extrema(uvrng)...)
     func == abs && plt.ylim(0)
-end
-
-function radplot(func::typeof(angle), c::ModelComponent, uvrng::AbstractInterval{<:Real}; n=100, alpha=0.2)
-    uvdists = range(extrema(uvrng)...; length=n)
-    vises = proper_ustrip.(func, visibility_envelope.(func, c, uvdists))
-    color = plt.gca()._get_lines.prop_cycler.__next__()["color"] |> mpl_color
-    plt.fill_between(uvdists, minimum.(vises), maximum.(vises); ec=color, fc=coloralpha(color, alpha))
-    plt.xlabel("UV distance (λ)")
-    plt.ylabel(proper_label(func))
-
-    ylim_shrink(0 ± 180)
-    plt.xlim(extrema(uvrng)...)
+    func == angle && lim_intersect(y=0 ± 180)
 end
 
 function radplot(funcs::Vector{<:Function}, args...; ax, kwargs...)
@@ -103,16 +92,6 @@ function radplot(funcs::Vector{<:Function}, args...; ax, kwargs...)
         ax[1].xaxis.tick_top()
         ax[1].xaxis.set_label_position(:top)
     end
-end
-
-function ylim_shrink(int)
-    l = Interval(plt.ylim()...)
-    int ⊆ l && plt.ylim(extrema(int))
-end
-
-function ylim_expand(int)
-    l = Interval(plt.ylim()...)
-    l ⊆ int && plt.ylim(extrema(int))
 end
 
 end
