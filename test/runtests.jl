32e4cca3d30294ff37ecdbebc78c89b56d8d8818
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    split radplot, more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,15 @@
 using VLBIPlots
 using InterferometricModels
+using IntervalSets
 using Test
 
 
-BeamArtist(beam(EllipticGaussian, σ_major=0.5, ratio_minor_major=0.5, pa_major=deg2rad(15)))
+comp = beam(EllipticGaussian, σ_major=0.5, ratio_minor_major=0.5, pa_major=deg2rad(15))
+
+BeamArtist(comp)
+
+radplot(abs, comp, 0..1e9)
+radplot(angle, comp, 0..1e9)
 
 
 import CompatHelperLocal as CHL
