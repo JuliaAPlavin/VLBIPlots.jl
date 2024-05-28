6af96ba543c50f5c324ca6c8ee04af343a192445
Alexander Plavin <alexander@plav.in>
2024-05-28 15:16:25 -0400

    fix circle/ellipse width

--- src/modelpoly.jl
+++ src/modelpoly.jl
@@ -5,11 +5,11 @@ Makie.convert_arguments(ct::Type{<:Poly}, m::InterferometricModels.Point) =
     convert_arguments(ct, Circle(Point2(ustrip.(coords(m))...), 0))
 
 Makie.convert_arguments(ct::Type{<:Poly}, m::CircularGaussian) =
-    convert_arguments(ct, Circle(Point2(ustrip.(coords(m))...), ustrip(fwhm_average(m))))
+    convert_arguments(ct, Circle(Point2(ustrip.(coords(m))...), ustrip(fwhm_average(m) / 2)))
 
 function Makie.convert_arguments(ct::Type{<:Poly}, m::EllipticGaussian)
     s, c = sincos(π/2 - position_angle(m))
-    trmat = [c -s; s c] * Diagonal([fwhm_max(m), fwhm_min(m)])
+    trmat = [c -s; s c] * Diagonal([fwhm_max(m), fwhm_min(m)] / 2)
     polygon = map(range(0, 2π, length=100)) do θ
         p_circ = SVector(sincos(θ))
         Point2(ustrip.(trmat * p_circ + coords(m))...)
