c512d13159c9e867bd3650e4db3710a65c5e21f3
Alexander Plavin <alexander@plav.in>
2024-06-19 21:05:12 -0400

    better beampoly

--- src/beamplot.jl
+++ src/beamplot.jl
@@ -1,11 +1,21 @@
-beampoly!(ax::Axis, b::ModelComponent; centerax, kwargs...) = 
-    poly!(
-        ax,
-        @lift begin
-            lo = SVector($(ax.finallimits).origin)
-            ws = SVector($(ax.finallimits).widths)
-            center_data = lo + SVector(centerax) .* ws
-            @modify(c -> c + center_data, coords(b))
-        end;
-        kwargs...
+@recipe(Beampoly, beam) do scene
+    Attributes(
+        position=Point2(0.1, 0.1),
     )
+end
+
+function Makie.plot!(p::Beampoly)
+    attrs = @p let
+        Makie.shared_attributes(p, Poly)
+    end
+    poly!(p, attrs,
+        @lift let
+            center_data = Makie.fractionpoint(
+                convert(Rect2f, $(Makie.projview_to_2d_limits(p))),
+                convert(Point2, $(p.position))
+            )
+            @modify(c -> c + center_data, coords($(p.beam)))
+        end
+    )
+    return p
+end
