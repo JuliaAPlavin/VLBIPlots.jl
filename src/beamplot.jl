b1bd20eaef6da658746c996ddd2fabfa4836f5dc
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    remove upstreamed fixes

--- src/beamplot.jl
+++ src/beamplot.jl
@@ -13,23 +13,3 @@ function Makie.plot!(p::BeamPoly{<:Tuple{ModelComponent}})
     poly!(p, Makie.shared_attributes(p, Poly), @lift @modify(c -> c + $center_data, coords($(p.beam))))
     return p
 end
-
-# XXX: https://github.com/MakieOrg/Makie.jl/pull/3909
-function __init__()
-    if ccall(:jl_generating_output, Cint, ()) != 1
-        @eval Makie function project(scenelike::SceneLike, input_space::Symbol, output_space::Symbol, pos::VecTypes{N, T1}) where {N, T1}
-            T = promote_type(Float32, T1) # always float, maybe Float64
-            input_space === output_space && return to_ndim(Point3{T}, pos, 0)
-            cam = camera(scenelike)
-
-            input_f32c = f32_convert_matrix(scenelike, input_space)
-            clip_from_input = space_to_clip(cam, input_space)
-            output_from_clip = clip_to_space(cam, output_space)
-            output_f32c = inv_f32_convert_matrix(scenelike, output_space)
-
-            p4d = to_ndim(Point4{T}, to_ndim(Point3{T}, pos, 0), 1)
-            transformed = output_f32c * output_from_clip * clip_from_input * input_f32c * p4d
-            return Point3{T}(transformed[Vec(1, 2, 3)] ./ transformed[4])
-        end
-    end
-end
--- src/uvplot.jl
+++ src/uvplot.jl
@@ -11,9 +11,3 @@ _uvfunc(uvscale) = function(uv)
     uv′ = @modify(uvscale, norm(uv))
     return inverse(uvscale).(uv′)
 end
-
-# XXX: piracy, https://github.com/JuliaObjects/Accessors.jl/pull/162
-function Accessors.set(arr, ::typeof(norm), val::Number)
-	omul = iszero(val) ? one(norm(arr)) : norm(arr)
-	map(Base.Fix2(*, val / omul), arr)
-end
