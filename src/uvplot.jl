2eb124eb2f9963381c518335f46665173e8ac4aa
Alexander Plavin <alexander@plav.in>
2025-08-05 04:43:02 -0400

    uvplot reverse x

--- src/uvplot.jl
+++ src/uvplot.jl
@@ -15,7 +15,7 @@ UVPlot(tbl; uvscale=identity, kwargs...) = UVPlot(FPlot(tbl); uvscale, kwargs...
 
 
 UVPlot(uvs::AbstractInterval; kwargs...) = UVPlot(uvs, uvs; kwargs...)
-UVPlot(us::AbstractInterval, vs::AbstractInterval; npix::Int=200, kwargs...) = UVPlot(range(us, length=npix), range(us, length=npix); kwargs...)
+UVPlot(us::AbstractInterval, vs::AbstractInterval; npix::Int=200, kwargs...) = UVPlot(range(us, length=npix)|>reverse, range(us, length=npix); kwargs...)
 UVPlot(us::AbstractRange, vs::AbstractRange; model, color=x -> abs(x.value)) = map(uv -> color((value=visibility(model)(uv), uv=uv)), grid(SVector, U=us, V=vs))
 
 
