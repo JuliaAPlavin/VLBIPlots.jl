4d2e6ec7b8191aa839075019bae304d569c016cf
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    add image UVPlot

--- src/uvplot.jl
+++ src/uvplot.jl
@@ -12,3 +12,8 @@ UVPlot(fplt::FPlot; uvscale=identity, kwargs...) = @p let
 end
 
 UVPlot(tbl; uvscale=identity, kwargs...) = UVPlot(FPlot(tbl); uvscale, kwargs...)
+
+
+UVPlot(uvs::AbstractInterval; kwargs...) = UVPlot(uvs, uvs; kwargs...)
+UVPlot(us::AbstractInterval, vs::AbstractInterval; npix::Int=200, kwargs...) = UVPlot(range(us, length=npix), range(us, length=npix); kwargs...)
+UVPlot(us::AbstractRange, vs::AbstractRange; model, color=x -> abs(x.value)) = map(uv -> color((value=visibility(model)(uv), uv=uv)), grid(SVector, U=us, V=vs))
